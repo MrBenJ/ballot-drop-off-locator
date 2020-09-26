88e511ea8f01ce6d4acca94a51c651445273006b
Ben Junya <bjunya@gmail.com>
2020-09-26 09:35:43 -0700

    Set test timeout to 5 minutes. Allow for full parsing of data before logging errors in tests

--- src/App.test.js
+++ src/App.test.js
@@ -25,99 +25,111 @@ const getCountyFile = async (state, file) => {
   return countyFile;
 };
 
-test('Validates index.json data', async () => {
-  const schema = {
-    id: '/Index',
-    type: 'array',
-    validateItems: {
-      items: {
-        type: 'object',
-        properties: {
-          adminarea: {
-            type: 'string'
-          },
-          shortname: {
-            type: 'string'
+const SECONDS = 1000;
+const MINUTES = 60 * SECONDS;
+const TEST_TIMEOUT_LENGTH = 5 * MINUTES;
+
+test(
+  'Validates index.json data',
+  async () => {
+    const schema = {
+      id: '/Index',
+      type: 'array',
+      validateItems: {
+        items: {
+          type: 'object',
+          properties: {
+            adminarea: {
+              type: 'string'
+            },
+            shortname: {
+              type: 'string'
+            }
           }
         }
       }
+    };
+    const v = new Validator();
+    v.addSchema(schema, '/Schema');
+
+    const dirs = await getDirectory('data');
+
+    for (let i = 0; i < dirs.length; i++) {
+      const stateDirectory = dirs[i];
+
+      const indexJson = await getIndexFile(stateDirectory);
+      const result = v.validate(indexJson, schema);
+
+      expect(result.errors).toHaveLength(0);
     }
-  };
-  const v = new Validator();
-  v.addSchema(schema, '/Schema');
+  },
+  TEST_TIMEOUT_LENGTH
+);
 
-  const dirs = await getDirectory('data');
-
-  for (let i = 0; i < dirs.length; i++) {
-    const stateDirectory = dirs[i];
-
-    const indexJson = await getIndexFile(stateDirectory);
-    const result = v.validate(indexJson, schema);
-
-    expect(result.errors).toHaveLength(0);
-  }
-});
-
-test('Validates individual state data', async () => {
-  /*
+test(
+  'Validates individual state data',
+  async () => {
+    /*
   "adminarea": "Albany",
   "urlvbmdropboxes": "http://app.albanycounty.com/boe/voterinfo/",
   "urlearlyvoting": "http://app.albanycounty.com/boe/voterinfo/",
   "flagboxes": 0,
   "shortname": "albany"
   */
-  const schema = {
-    id: '/State',
-    type: 'object',
-    properties: {
-      adminarea: {
-        type: 'string'
-      },
-      shortname: {
-        type: 'string'
-      },
-      urlvbmdropboxes: {
-        type: 'string'
-      },
-      urlearlyvoting: {
-        type: ['string', 'null']
-      },
-      flagboxes: {
-        type: 'number'
+    const schema = {
+      id: '/State',
+      type: 'object',
+      properties: {
+        adminarea: {
+          type: 'string'
+        },
+        shortname: {
+          type: 'string'
+        },
+        urlvbmdropboxes: {
+          type: 'string'
+        },
+        urlearlyvoting: {
+          type: ['string', 'null']
+        },
+        flagboxes: {
+          type: 'number'
+        }
+      }
+    };
+
+    const v = new Validator();
+    v.addSchema(schema, '/StateSchema');
+
+    const dirs = await getDirectory('data');
+    const errors = [];
+    for (let i = 0; i < dirs.length; i++) {
+      const stateDirectoryName = dirs[i];
+      const stateDirectoryFiles = await getDirectory(
+        `data/${stateDirectoryName}`
+      );
+
+      for (let j = 0; j < stateDirectoryFiles.length; j++) {
+        const countyFilename = stateDirectoryFiles[j];
+
+        if (countyFilename === 'index.json') {
+          continue;
+        }
+        const file = await getCountyFile(stateDirectoryName, countyFilename);
+
+        const result = v.validate(file, schema);
+
+        if (result.errors.length > 0) {
+          errors.push(
+            `ERROR in State: ${stateDirectoryName.toUpperCase()} FILE: ${countyFilename}`
+          );
+        }
       }
     }
-  };
-
-  const v = new Validator();
-  v.addSchema(schema, '/StateSchema');
-
-  const dirs = await getDirectory('data');
-  for (let i = 0; i < dirs.length; i++) {
-    const stateDirectoryName = dirs[i];
-
-    const stateDirectoryFiles = await getDirectory(
-      `data/${stateDirectoryName}`
-    );
-
-    for (let j = 0; j < stateDirectoryFiles.length; j++) {
-      const countyFilename = stateDirectoryFiles[j];
-
-      if (countyFilename === 'index.json') {
-        continue;
-      }
-      const file = await getCountyFile(stateDirectoryName, countyFilename);
-
-      const result = v.validate(file, schema);
-
-      if (result.errors.length > 0) {
-        console.log(
-          'ERROR in State: ',
-          stateDirectoryName,
-          'FILE: ',
-          countyFilename
-        );
-      }
-      expect(result.errors).toHaveLength(0);
+    if (errors.length > 0) {
+      console.log(JSON.stringify(errors, null, 2));
     }
-  }
-});
+    expect(errors).toHaveLength(0);
+  },
+  TEST_TIMEOUT_LENGTH
+);
