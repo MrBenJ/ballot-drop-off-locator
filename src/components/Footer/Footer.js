78d36274bc80c396fe5e0fb3823de0be3ab689af
Ben Junya <bjunya@gmail.com>
2020-09-26 11:13:50 -0700

    Create Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import cn from 'classnames';
+import { useTranslation } from 'react-i18next';
 
+import Text from '../Text';
 import './Footer.scss';
+
+// TODO: Add social media icons
+
+const Divider = () => {
+  return (
+    <Text className="divider" variant="span">
+      |
+    </Text>
+  );
+};
 
 const Footer = props => {
   const { className } = props;
+  const { t } = useTranslation();
 
-  return <div className={cn('Footer', className)}>Footer</div>;
+  return (
+    <div className={cn('Footer', className)}>
+      <Text className="footer-ballot-drop center">
+        {t('Footer.MyBallotDrop')}
+      </Text>
+      <div className="social-icons">{/* ADD SOCIAL MEDIA ICONS HERE */}</div>
+      <div className="footer-links-list">
+        <Text variant="span">
+          {t('Footer.linkAboutProject')}
+          <Divider />
+          {t('Footer.linkDataIntegrity')}
+          <Divider />
+          {t('Footer.linkContactUs')}
+        </Text>
+      </div>
+      <Text className="voter-info-project center">
+        {t('Footer.linkVoterInfo')}
+      </Text>
+    </div>
+  );
 };
 
 export default Footer;
