815e3ea7650518a2639c689ed0d68afbeb517098
Ben Junya <bjunya@gmail.com>
2020-10-05 21:01:17 -0700

    Get Coburn's blessing on desktop landing page (#10)
    * Get Coburn's blessing on desktop landing page
    
    * Change spacing on text for mobile and desktop

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-import { ReactComponent as LocateSVG } from './locate.svg';
+import { ReactComponent as DropIconSVG } from './dropIcon.svg';
 import Button from '../../components/Button';
 import Text from '../../components/Text';
 
-import MainImage from './mainImage.jpg';
-import MainImage2x from './mainImage@2x.jpg';
-import MainImage3x from './mainImage@3x.jpg';
+import dropBoxHero2x from './dropBoxHero@2x.jpg';
+import dropBoxHero3x from './dropBoxHero@3x.jpg';
 
 import './IndexPage.scss';
 
@@ -17,56 +16,64 @@ const IndexPage = () => {
 
   return (
     <div className="container">
-      <img
-        src={MainImage}
-        className="main-image"
-        alt={t('IndexPage.imgAlt')}
-        srcSet={`${MainImage2x} 2x, ${MainImage3x} 3x`}
-      />
       <div className="IndexPage">
-        <Text className="dropbox-bolded bold center">
-          {t('IndexPage.findBoxBold')}
-        </Text>
-        <Text className="dropbox-copy center">
-          {t('IndexPage.absenteeText')}
-        </Text>
-        <Text className="dropbox-copy center">
-          {t('IndexPage.aboutMyBallotDrop')}
-        </Text>
-        <Link to="/select-state">
-          <Button className="button-cta">{t('IndexPage.buttonCTA')}</Button>
-        </Link>
-        <Text className="locate-copy-wrapper center">
-          <Text className="bold" variant="span">
-            {t('IndexPage.boldLocate')}
-          </Text>{' '}
-          {t('IndexPage.whereAvailable')}{' '}
-          <Text className="bold" variant="span">
-            {t('IndexPage.boldDropDirectly')}
-          </Text>{' '}
-          {t('IndexPage.atJursidiction')}
-        </Text>
-        <Text className="ensure-text-teal center bold">
-          {t('IndexPage.tealBolded')}
-          <br />
-          {t('IndexPage.tealBolded2')}
-        </Text>
-        <div className="locate-svg">
-          <Link to="/select-state">
-            <LocateSVG />
-          </Link>
+        <div className="top-flex">
+          <div className="top-flex-first">
+            <img
+              src={dropBoxHero2x}
+              className="main-image top-flex-first"
+              alt={t('IndexPage.imgAlt')}
+              srcSet={`${dropBoxHero2x} 2x, ${dropBoxHero3x} 3x`}
+            />
+          </div>
+          <div className="top-flex-second">
+            <Text className="dropbox-bolded bold center">
+              {t('IndexPage.findBoxBold')}
+            </Text>
+            <Text className="dropbox-copy center">
+              {t('IndexPage.absenteeText')}
+            </Text>
+            <Link to="/select-state">
+              <Button className="button-cta">{t('IndexPage.buttonCTA')}</Button>
+            </Link>
+            <Text className="dropbox-copy center">
+              {t('IndexPage.aboutMyBallotDrop')}
+            </Text>
+            <Text className="locate-copy-wrapper center">
+              <Text className="bold" variant="span">
+                {t('IndexPage.boldLocate')}
+              </Text>{' '}
+              {t('IndexPage.whereAvailable')}{' '}
+              <Text className="bold" variant="span">
+                {t('IndexPage.boldDropDirectly')}
+              </Text>{' '}
+              {t('IndexPage.atJursidiction')}
+            </Text>
+          </div>
         </div>
-        <Text className="county-gov-copy center">
-          {t('IndexPage.siteProvides')}{' '}
-          <Text className="bold" variant="span">
-            {t('IndexPage.boldedCounty')}
-          </Text>{' '}
-          {t('IndexPage.or')}{' '}
-          <Text className="bold" variant="span">
-            {t('IndexPage.boldedStateGov')}
-          </Text>{' '}
-          {t('IndexPage.siteProvidesEnd')}
-        </Text>
+        <div className="content">
+          <Text className="ensure-text-teal center bold">
+            {t('IndexPage.tealBolded')}
+            <br />
+            {t('IndexPage.tealBolded2')}
+          </Text>
+          <div className="locate-svg">
+            <Link to="/select-state">
+              <DropIconSVG />
+            </Link>
+          </div>
+          <Text className="county-gov-copy center">
+            {t('IndexPage.siteProvides')}{' '}
+            <Text className="bold" variant="span">
+              {t('IndexPage.boldedCounty')}
+            </Text>{' '}
+            {t('IndexPage.or')}{' '}
+            <Text className="bold" variant="span">
+              {t('IndexPage.boldedStateGov')}
+            </Text>{' '}
+            {t('IndexPage.siteProvidesEnd')}
+          </Text>
+        </div>
       </div>
     </div>
   );
