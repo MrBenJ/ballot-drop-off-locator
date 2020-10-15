67107174da5db9920271ad455ccacbded85e6acf
Ben Junya <bjunya@gmail.com>
2020-10-15 16:11:26 -0700

    Add links for social media icons (#11)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,9 @@ import cn from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
+import { ReactComponent as FacebookSVG } from './fb.svg';
+import { ReactComponent as TwitterSVG } from './twitter.svg';
+import { ReactComponent as InstaSVG } from './insta.svg';
 import Text from '../Text';
 import './Footer.scss';
 
@@ -25,7 +28,32 @@ const Footer = props => {
       <Text className="footer-ballot-drop center">
         {t('Footer.MyBallotDrop')}
       </Text>
-      <div className="social-icons">{/* ADD SOCIAL MEDIA ICONS HERE */}</div>
+      <div className="social-icons">
+        <div className="social-icons-item">
+          <a
+            href="https://www.facebook.com/myballotdrop/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <FacebookSVG className="svg svg-facebook" />
+          </a>
+        </div>
+        <div className="social-icons-item">
+          <a
+            href="https://twitter.com/MyBallotDrop"
+            target="_blank"
+            rel="noopener noreferrer">
+            <TwitterSVG className="svg svg-twitter" />
+          </a>
+        </div>
+        <div className="social-icons-item">
+          <a
+            href="https://www.instagram.com/myballotdrop/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <InstaSVG className="svg svg-insta" />
+          </a>
+        </div>
+      </div>
       <div className="footer-links-list">
         <Text variant="span">
           <Link to="/about">{t('Footer.linkAboutProject')}</Link>
